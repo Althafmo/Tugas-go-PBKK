fe26336b76c21ad2fbb4db36de2cde11f1e1d6f7
Althafmo <144194095+Althafmo@users.noreply.github.com>
2024-11-04 14:32:37 +0700

    Update univers.go

--- univers.go
+++ univers.go
@@ -1,1 +1,18 @@
+package main
 
+import "fmt"
+
+func main() {
+	var input int
+	fmt.Printf("Masukkan angka: ")
+	_, err := fmt.Scan(&input)
+
+	if err != nil {
+		fmt.Println("Input tidak valid")
+		return
+	} else if input == 42 {
+		fmt.Println("Hello Universe")
+	} else {
+		fmt.Printf("Anda memasukkan angka: %d\n", input)
+	}
+}
